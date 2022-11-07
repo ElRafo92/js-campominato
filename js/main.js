0ad89c3dd35c899ba512c12b4505727f4a2fe96b
ElRafo92 <rafael.martinez@outlook.it>
2022-11-07 22:02:26 +0100

    finito

--- js/main.js
+++ js/main.js
@@ -8,55 +8,78 @@ function generatoreTabella() {
 genera.addEventListener('click', function() {
     let difficolta = document.getElementById('difficolta').value;
     tabella.innerHTML = '';
+    const score = [];
 
     for (let i = 1; i <= difficolta; i++) {
-    const casella = document.createElement('div');
-    casella.classList.add('casella', 'bg-celes');
-    casella.innerHTML = i;
+        const casella = document.createElement('div');
+        casella.classList.add('casella', 'bg-celes');
+        casella.innerHTML = i;
+       // console.log(casella);
 
-    if (difficolta == '100') {
-        casella.classList.add('casella-width1')
-    } else if (difficolta == '81') {
-        casella.classList.add('casella-width2')
-    } else {
-        casella.classList.add('casella-width3')
-    }
+        if (difficolta == '100') {
+            casella.classList.add('casella-width1')
+        } else if (difficolta == '81') {
+            casella.classList.add('casella-width2')
+        } else {
+            casella.classList.add('casella-width3')
+        }
+        
+        casella.addEventListener('click', function() {
+            if (bombe.includes(i)) {
+                casella.classList.remove('bg-celes');
+                casella.classList.add('bg-red');
+                alert(`Hai perso! La pagina verrà ricaricata. Il tuo punteggio è di: ${score.length}`);
+                location.reload(window);
+            } else if (!score.includes(i)) {
+                casella.classList.remove('bg-celes');
+                casella.classList.add('bg-green');
+                score.push(i)
+            } 
+            console.log(score);
+            console.log(difficolta);
 
-    casella.addEventListener('click', function() {
-        casella.classList.remove('bg-celes');
-        casella.classList.add('bg-green');
-    })
+            if (score.length === difficolta - 16) {
+                alert(`Hai vinto! Per giocare ancore clicca su RESET. Il tuo punteggio è di: ${score.length}`);
+            }
+            
+            console.log(i - 16);
 
-    tabella.append(casella);
+        });
+
+        tabella.append(casella);
     };
+    let totCell = 16;
+    ;
+    let bombe = [];
+    uniqueRandomGenerator(bombe, totCell);
+    console.log(bombe);
+
+
+
+
 });
 
 reset.addEventListener('click', function(){
     location.reload(window)
-})
+});
 };
 
 tabella.innerHTML = generatoreTabella();
 
-let difficolta = Number(document.getElementById('difficolta').value);
-function uniqueRandomGenerator() {
-let bombe = [];
-let indexBombe = 0;
-console.log(typeof difficolta);
-    if (difficolta == 100) {
-        while ( bombe.length < 100 ) {
-            let x = Math.floor((Math.random() * 100) + 1);
-            if (bombe.indexOf(x) === -1) {
-                bombe.push(x);
-            }
-            indexBombe++
-        }
-    }
 
-return bombe
+/* =========================
+   =========================
+            FUNCTIONS
+   =========================
+   ========================= */
+function uniqueRandomGenerator(insieme, cap) {
+
+        while ( insieme.length < 16 ) {
+            let x = Math.floor((Math.random() * cap) + 1);
+            if (insieme.indexOf(x) === -1) {
+                insieme.push(x);
+            };
+        };
+
+return insieme
 };
-
-console.log(difficolta);
-
-console.log(uniqueRandomGenerator())
-
